58a4286a025658e65a34d005f564a66958464645
HeppokoNeet <tatsuru.daime@i-standard.jp>
2021-04-21 14:12:35 +0900

    エラの時の表示

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,16 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get "/auth/:provider/callback" => "sessions#create"
   delete "/logout" => "sessions#destroy"
-  resource :retirements
+  resource :retirements, only: %i[new create]
+
   get 'status' => 'status#index', defaults: { format: 'json' }
 
 
-  resources :events do
-    resources :tickets
+  resources :events, only: %i[new create show edit update destroy] do
+    resources :tickets, only: %i[new create destroy]
   end
+
+  match "*path" => "application#error404", via: :all
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
